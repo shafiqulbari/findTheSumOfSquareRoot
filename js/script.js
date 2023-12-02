fd32a8fbd747a7088d946b5d525c2e5d63dcc6cb
Shafiqul Bari <120185849+shafiqulbari@users.noreply.github.com>
2023-12-02 22:43:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// method01
+
+// var n = parseInt(prompt("Enter your desired number: "));
+
+// var sum = 0;
+// var output = "";
+
+// for (var i = 1; i <= n; i++) {
+//     var squared = i ** 2;
+//     output += squared;
+//     if (i < n) {
+//         output += " + ";
+//     } else {
+//         output += " = ";
+//     }
+//     sum += squared;
+// }
+
+// console.log(output + sum);
+
+// method02
+
+var n = parseInt(prompt("Enter your desired number: "));
+var sum = 0;
+var output = "";
+
+for (var i = 1; i <= n; i++){
+    sum += i*i;
+    output += (i*i).toString();
+    console.log (output);
+    if (i==n){
+        continue
+    }
+    output += " + ";
+}
+
+console.log(`${output} = ${sum}`)
